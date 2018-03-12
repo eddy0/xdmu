a2e7b756cd593f09f4e26da8a4ffae9e960636e4
eddy0 <awindy369@gmail.com>
2018-03-12 12:43:52 -0700

    finish todo
    not yet add ajax

--- templates/todo/calendarEvent.js
+++ templates/todo/calendarEvent.js
@@ -5,6 +5,8 @@ const container = () => {
     const contentYearContainer = e('.content-year')
     const contentMonthContainer = e('.content-month')
     const contentDateContainer = e('.content-date')
+    const contentDayContainer = e('.content-day')
+    const contentDateWrapper = e('.date-wrapper')
     return {
         yearContainer,
         monthContainer,
@@ -12,148 +14,179 @@ const container = () => {
         contentYearContainer,
         contentMonthContainer,
         contentDateContainer,
+        contentDayContainer,
+        contentDateWrapper,
     }
 }
 
-const highlightMonth = () => {
-    const {monthContainer} = container()
-    const m = monthContainer
-    m.addEventListener('click', (event) => {
-        const target = event.target
-        const sel = e('.selected', m)
-        sel.classList.remove('selected')
-        target.classList.add('selected')
-        dateUpdate()
+const currentHightlightTime = () => {
+    const {yearContainer, monthContainer, dateContainer} = container()
+    //current year
+    const selectedYear = yearContainer.innerText
+    //current month
+    const selectedMonth = e('.selected', monthContainer).dataset.month
+    // current date
+    const selectedDate = e('.active', dateContainer).innerText
+    const year = Number(selectedYear)
+    const month =  Number(selectedMonth)
+    const date =  Number(selectedDate)
+    // current date
 
-        loadReminder()
-        insertRemindermark()
+    const day = new Date(year, month, date).getDay()
 
-    })
+    return {year, month, date, day}
 }
 
-const hightlightDate = () => {
-    const d = e('.date-lists')
-    d.addEventListener('click', (event) => {
-        const target = event.target
-        const p = target.parentElement
-        const list = p.classList.contains('date-lists') || p.parentElement.classList.contains('date-lists')
-        if (list) {
-            const sel = target.closest('.current-dateList')
-            const wrapper = e('.date-lists')
-            const activedElement = e('.active', wrapper)
-            if (activedElement !== null) {
-                activedElement.classList.remove('active')
-            }
-            sel.classList.add('active')
-
-            contentUpdate()
-
-            loadReminder()
-
-            insertRemindermark()
-
-        }
-    })
+const dateUpdate = () => {
+    const {year, month, date} = currentHightlightTime()
+    // calendar date update
+    mapUpdate(year, month, date)
+    // reminder date update
+    contentUpdate()
 }
 
-const yearUpdate = (year) => {
-    const yearContainer = e('.currentYear')
-    yearContainer.innerText = year
-}
-
-const hightlightYear = () => {
-    let currentYear = Number(e('.currentYear').innerText)
+const yearEvent = () => {
+    const {yearContainer} = container()
 
     const pre = e('.previous')
     pre.addEventListener('click', () => {
-        currentYear -= 1
-        yearUpdate(currentYear)
-        dateUpdate()
+        let {year, month, date} = currentHightlightTime()
+        year -= 1
+        yearContainer.innerText = year
 
-        loadReminder()
-        insertRemindermark()
-
+        reload(year, month, date)
 
     })
 
     const next = e('.next')
     next.addEventListener('click', () => {
-        currentYear += 1
-        yearUpdate(currentYear)
-        dateUpdate()
+        let {year, month, date} = currentHightlightTime()
+        year += 1
+        yearContainer.innerText = year
 
-        loadReminder()
-        insertRemindermark()
-
+        reload(year, month, date)
 
     })
 }
 
-const dateUpdate = () => {
-    const {yearContainer, monthContainer, dateContainer} = container()
-    const selectedYear = yearContainer.innerText
-    const selectedMonth = e('.selected', monthContainer).dataset.month
-    const selectedDate = e('.active', dateContainer).innerText
+const monthEvent = () => {
+    const {monthContainer} = container()
 
-    const year = Number(selectedYear)
-    const month =  Number(selectedMonth)
-    const date =  Number(selectedDate)
-    mapUpdate(year, month, date)
+    const m = monthContainer
+    m.addEventListener('click', (event) => {
+        // highlight date update
+        const target = event.target
+        const sel = e('.selected', m)
+        sel.classList.remove('selected')
+        target.classList.add('selected')
 
-    // get day
-    contentUpdate()
+        dateUpdate()
 
+        // load new data
+        loadReminderData()
 
+        initMark()
 
-    return {year, month, date}
+        // update reminder mark
+        ResponseByData()
+
+    })
 }
 
-const template = (dateCount, weekday) => {
+const dateEvent = () => {
+    const dateContainer = e('.date-lists')
+
+    dateContainer.addEventListener('click', (event) => {
+        const target = event.target
+        const p = target.parentElement
+        const list = p.classList.contains('date-lists') || p.parentElement.classList.contains('date-lists')
+        if (list) {
+            // highlight date update
+            const sel = target.closest('.current-dateList')
+            const activedElement = e('.active', dateContainer)
+            if (activedElement !== null) {
+                activedElement.classList.remove('active')
+            }
+            sel.classList.add('active')
+
+            // reminder date update
+            contentUpdate()
+
+            // data update
+            loadReminderData()
+
+            // insertRemindermark()
+            ResponseByData()
+
+        }
+    })
+}
+
+const dateTemplate = (dateCount, weekday) => {
     let ts = `
-             <li class="current-dateList" style="margin-left: ${71.42 * weekday}px"><span>1</span></li>\n
+             <li class="current-dateList" style="margin-left: ${71.42 * weekday}px">
+                 <span class="current-date">1</span>
+             </li>\n
              `
     for (let i = 2; i <= dateCount ; i++) {
         let t = `
                 <li class="current-dateList">
-                <span class="current-date">${i}</span>
-                <!--<svg class="icon icon-reminder" fill="#000" height="20" style="text-align: center" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path></svg>-->
-                <!--</li>\n-->`
+                    <span class="current-date">${i}</span>
+                </li>
+               `
+        /*
+         <svg class="icon icon-reminder" fill="#000" height="20" style="text-align: center" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path></svg>
+
+
+         */
         ts += t
     }
     return ts
 }
 
-/*
-const reminderMark = () => {
-    todo: get ajax data and if the date has a reminder, then insert the svg template below to hint there is a reminder, (feature: if over 3 reminders, the fill color will be various)
-
-    <svg class="icon icon-reminder" fill="#000" height="20" style="text-align: center" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path></svg>
-
-
-}
-*/
-const insertTemplate = (dateCount, day, date) => {
+const insertDateTemplate = (dateCount, day) => {
     const {dateContainer} = container()
     dateContainer.innerHTML = ''
-    // const  t = template(dateCount, day, date)
-    const  t = template(dateCount, day)
+    const  t = dateTemplate(dateCount, day)
     dateContainer.insertAdjacentHTML('beforeend', t )
+}
 
-    // highlight date
-    if (date > dateCount) {
-        date = dateCount
+const daysMapper = (year, month) => {
+    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if (year % 100 === 0 && year % 400 === 0) {
+        days[1] = 29
     }
-    dateContainer.children[date - 1].classList.add('active')
+    if (year % 100 !== 0 && year % 4 === 0) {
+        days[1] = 29
+    }
+    const dayCountByMonth = days[month]
 
+    // type number
+    const firstDay = new Date(year, month, 1)
+    const firstDayByMonth = firstDay.getDay()
+    return {dayCountByMonth, firstDayByMonth}
+}
 
+const highlightUpdate = (date) => {
+    const {dateContainer} = container()
+    const dateCount = dateContainer.children.length
+    let index = date
+    if (index > dateCount) {
+        index = dateCount
+    }
+    dateContainer.children[index - 1].classList.add('active')
+}
+
+const mapUpdate = (year, month, date) => {
+    const {dayCountByMonth, firstDayByMonth} = daysMapper(year, month)
+    insertDateTemplate(dayCountByMonth, firstDayByMonth, date)
+    highlightUpdate(date)
 }
 
 const contentYearUpdate = () => {
-    const highlightedYear = e('.currentYear').innerText
-    const year = Number(highlightedYear)
-    const contentYear = e('.content-year')
-    contentYear.innerText = year
-    return year
+    let {year} = currentHightlightTime()
+    const {contentYearContainer} = container()
+    contentYearContainer.innerText = year
 }
 
 const contentMonthUpdate = () => {
@@ -171,11 +204,9 @@ const contentMonthUpdate = () => {
         10: 'NOVEMBER',
         11: 'DECEMBER',
     }
-    const contentMonthContainer = e('.content-month')
-    const highlightedMonth = e('.selected')
-    let m = Number(highlightedMonth.dataset.month)
-    contentMonthContainer.innerHTML = monthMap[m]
-    return m
+    const {contentMonthContainer} = container()
+    const {month} = currentHightlightTime()
+    contentMonthContainer.innerHTML = monthMap[month]
 }
 
 const contentDateUpdate = () => {
@@ -185,18 +216,11 @@ const contentDateUpdate = () => {
         3: 'ST'
     }
 
-    /*
-    let orderedDate = `${date}TH`
-    if ( date in dateMap) {
-        orderedDate = String(date) + dateMap[date]
-    }
-    */
-    const highlightedDate = e('.active')
-    const date = Number(highlightedDate.innerText)
+    const {date} = currentHightlightTime()
     let orderedDate = (date in dateMap)? `${date}${dateMap[date]}`: `${date}TH`
-    const contentDateContainer = e('.content-date')
+    const {contentDateContainer} = container()
     contentDateContainer.innerText = orderedDate
-    return date
+
 }
 
 const contentDayUpdate = () => {
@@ -209,109 +233,31 @@ const contentDayUpdate = () => {
         5: 'FRIDAY',
         6: 'SATURDAY',
     }
-    const year = contentYearUpdate()
-    const month = contentMonthUpdate()
-    const date = contentDateUpdate()
-    const day = new Date(year, month, date).getDay()
-    const contentDateContainer = e('.content-day')
-    contentDateContainer.innerHTML = dayMap[day]
+    const {day} = currentHightlightTime()
+    const {contentDayContainer} = container()
+    contentDayContainer.innerHTML = dayMap[day]
 }
 
-const daysMapper = (year, month) => {
-    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if (year % 100 === 0 && year % 400 === 0) {
-        days[1] = 29
-    }
-    if (year % 100 !== 0 && year % 4 === 0) {
-        days[1] = 29
-    }
-    const dayCountByMonth = days[month]
-
-    // type number
-    const firstDay = new Date(year, month, 1)
-    const firstDayOfWeek = firstDay.getDay()
-    return {dayCountByMonth, firstDayOfWeek}
-}
-
-const mapUpdate = (year, month, date) => {
-    const {dayCountByMonth, firstDayOfWeek} = daysMapper(year, month)
-    insertTemplate(dayCountByMonth, firstDayOfWeek, date)
-}
-
-const highlightEvent = () => {
-    highlightMonth()
-    hightlightDate()
-    hightlightYear()
+const calendarEvents = () => {
+    yearEvent()
+    monthEvent()
+    dateEvent()
 }
 
 const contentUpdate = () => {
     contentYearUpdate()
     contentMonthUpdate()
     contentDateUpdate()
-    divResponse()
-
+    contentDayUpdate()
 }
 
-
-const dynamicResponse = () => {
-    insertRemindermark()
-    divResponse()
-}
-
-const divResponse = () => {
-    const items = e('.reminder-items')
-    const h = items.offsetHeight
-    const wrapper = e('.date-wrapper')
-    if (h > 245) {
-        wrapper.style.marginTop = '-50px'
-        wrapper.style.fontSize = '22px'
-
-    } else {
-        wrapper.style.marginTop = '50px'
-        wrapper.style.fontSize = 'inherit'
-
-
-    }
-}
-
-
-const init = () => {
-    // type number
-    const {year, month, date, day} = formattedTime()
-    // init year
-    const {yearContainer, monthContainer} = container()
-    yearContainer.innerHTML = year
-
-    // init highlighted date
-    const ms = monthContainer.querySelectorAll('.month-detail')
-    for (let i = 0; i < ms.length; i++) {
-        const index = ms[i].dataset.month
-        if (index === String(month)) {
-            ms[i].classList.add('selected')
-        }
-    }
-
-    // init date map, how many days in current month & year and the day for the first month
-    mapUpdate(year, month, date)
-
-
-    // init reminder content
-    contentUpdate(year, month, date, day)
-
-
-    // load data
-    loadReminder()
-
-    dynamicResponse()
-
-
-
+const dateEvents = () => {
+    calendarEvents()
+    contentUpdate()
 }
 
 
 
-// const calendarEvents = () => {
-//     init()
-//     highlightEvent()
-// }
+
+
 
--- templates/todo/reminder.js
+++ templates/todo/reminder.js
@@ -1,4 +1,4 @@
-const todoElementContainer = () => {
+const reminderContainer = () => {
     const addButton = e('.add-item-button')
     const addContainer = e('.add-item-container')
     const newContentContainer = e('.add-item-content')
@@ -12,8 +12,22 @@ const todoElementContainer = () => {
         }
 }
 
+const addContainerToggle = () => {
+    const {addButton, addContainer , newContentContainer} = reminderContainer()
+    // open add container
+    addButton.addEventListener('click',  () => {
+        if (addContainer.classList.contains('open')) {
+            addContainer.classList.remove('open')
+            newContentContainer.value=''
+        } else {
+            addContainer.classList.add('open')
+            newContentContainer.focus()
+        }
+    })
+}
+
 const undoAdd = () => {
-    const {addContainer, newContentContainer} = todoElementContainer()
+    const {addContainer, newContentContainer} = reminderContainer()
     if (addContainer.classList.contains('open')) {
         addContainer.classList.remove('open')
         newContentContainer.value=''
@@ -21,8 +35,10 @@ const undoAdd = () => {
     }
 }
 
-const todoTemplate = (reminder) => {
-    let task = reminder.task
+const todoTemplate = (data) => {
+    // data = {id : 1, reminder: {task:'', done:true, _delete:false,}}
+    const reminder = data.reminder
+    const task = reminder.task
     let status = {
         checked: '',
         done: '',
@@ -33,7 +49,7 @@ const todoTemplate = (reminder) => {
         }
 
     const t = `
-            <div class="reminder-item ">
+            <div class="reminder-item "  data-index=${data.id}>
                 <input type="checkbox" class="item-status ${status.checked}">
             <span class="item-content ${status.done}" contenteditable="false">${task}</span>
             <div class="item-actions">
@@ -45,29 +61,67 @@ const todoTemplate = (reminder) => {
     return t
 }
 
-const insertTodoTemplate = (reminder) => {
-    const {todoContainer} = todoElementContainer()
-    const t = todoTemplate(reminder)
+const insertTodoTemplate = (data) => {
+    const {todoContainer} = reminderContainer()
+    const t = todoTemplate(data)
     todoContainer.insertAdjacentHTML('beforeend', t)
+    undoAdd()
 }
 
-const todoContentUpdate = (content) => {
-    const keys = ['Enter', 'Escape']
-    listen(content, 'keydown', event => {
-        for (let k of keys) {
-            if (event.key === k) {
+const todoContentUpdate = (container) => {
+    const keys = {
+        Enter: 'Enter',
+        Escape: 'Escape',
+    }
+    listen(container, 'keydown', event => {
+        let key = event.key
+            if (key === keys[key]) {
                 event.preventDefault()
-                content.blur()
-                content.setAttribute('contentEditable', false)
+                container.blur()
+                container.setAttribute('contentEditable', false)
+
                 // todo edit data update
             }
-        }
+            if (key === 'Enter') {
+                const p = container.parentElement
+                const id = Number(p.dataset.index)
+                const task = container.innerText
+                const form = {
+                    id: id,
+                    reminder : {
+                        task: task,
+                        done: false,
+                        _delete: false,
+                        }
+                    }
+                updateData(form)
+            }
+
     })
 }
 
+const todoDoneupdate = (container, content) => {
+    const p = container.parentElement
+    const id = Number(p.dataset.index)
+    const task = content.innerText
+    let  done = false
+    if (container.classList.contains('checked')) {
+        done = true
+    }
+    const form = {
+        id: id,
+        reminder : {
+            task: task,
+            done: done,
+            _delete: false,
+        }
+    }
+    updateData(form)
+
+}
 // edit & remove & done
-const todoContentEdit = () => {
-    const {todoContainer} = todoElementContainer()
+const todoUpdate = () => {
+    const {todoContainer} = reminderContainer()
 
     listen(todoContainer, 'click', event => {
         const target = event.target
@@ -93,8 +147,16 @@ const todoContentEdit = () => {
         // delete
         const remove = target.classList.contains('icon-delete') || p.classList.contains('icon-delete')
         if (remove) {
+            const index = contentCell.dataset.index
+            // console.log('index,', index)
+
             contentCell.remove()
-            // todo remove data update
+
+            // remove data
+            removeData(index)
+
+            ResponseByData()
+
         }
 
         // done
@@ -111,13 +173,14 @@ const todoContentEdit = () => {
                 target.classList.add('checked')
             }
             // todo: done update
+            todoDoneupdate(target, content)
         }
 
     })
 }
 
-const todoContentAdd = () => {
-    const {newContentContainer} = todoElementContainer()
+const todoAdd = () => {
+    const {newContentContainer} = reminderContainer()
     // keydown event
     listen(newContentContainer, 'keyup', event => {
         // console.log(event.key)
@@ -126,16 +189,14 @@ const todoContentAdd = () => {
             const reminder = {
                 task: value,
                 done: false,
-                }
+                '_delete': false,
+            }
 
-            insertTodoTemplate(reminder)
+            const data = createData(reminder)
 
-            saveData(reminder)
+            insertTodoTemplate(data)
 
-            dynamicResponse()
-
-            undoAdd()
-
+            ResponseByData()
 
         }
         if (event.key === 'Escape') {
@@ -144,245 +205,10 @@ const todoContentAdd = () => {
     })
 }
 
-const addContainerToggle = () => {
-    const {addButton, addContainer , newContentContainer} = todoElementContainer()
-    // open add container
-    addButton.addEventListener('click',  () => {
-        if (addContainer.classList.contains('open')) {
-            addContainer.classList.remove('open')
-            newContentContainer.value=''
-        } else {
-            addContainer.classList.add('open')
-            newContentContainer.focus()
-        }
-    })
-}
-
-/*
-calendar 
-    calendar: [ 
-                {   id : 1,
-                    unixTime: 1234,
-                    userId: 1,
-                    task: 
-                              {'iifj',
-                                done: true,
-                                _deleted: false,
-                               },
-                }
-              ]
- */
-
-const reminderTemplate = (container, len) => {
-    let color = '#d0d000'
-    if (len > 3) {
-        color = '#39c9bb'
-    }
-    if (len > 8) {
-        color = '#dd6262'
-    }
-
-
-    const t = `
-      <svg class="icon icon-reminder" fill=${color} height="20" style="text-align: center" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
-      </svg>
-    `
-    const svg = e('.icon-reminder', container)
-    if (svg === null) {
-        container.insertAdjacentHTML('beforeend', t)
-    }
-}
-
-const insertRemindermark = () => {
-    //  only for current year & month
-
-    /*
-    let datas = loadReminderMark()
-    const {year, month} = dateUpdate()
-    const dateContainer = er('.current-dateList')
-
-    datas = datas.map( ds => new Date(ds))
-    datas.forEach( ds => {
-        const markYear = ds.getFullYear()
-        const markMonth = ds.getMonth()
-        const markDate = ds.getDate()
-        if (markYear === year && markMonth === month) {
-            dateContainer.forEach( date => {
-                let d = Number(date.innerText)
-                if (d === markDate) {
-                    reminderTemplate(date)
-                }
-            })
-        }
-    })
-    */
-    let data = ttt()
-    const {year, month} = dateUpdate()
-    const dateContainer = er('.current-dateList')
-
-
-    for (let newData in data ) {
-        // console.log('tasks', newData)
-
-        // let dt = Object.keys(newData)
-        let ds = new Date(Number(newData))
-
-        const markYear = ds.getFullYear()
-        const markMonth = ds.getMonth()
-        const markDate = ds.getDate()
-        const tasks = data[newData]
-        const len = tasks.length
-        if (markYear === year && markMonth === month) {
-            dateContainer.forEach( date => {
-                let d = Number(date.innerText)
-                if (d === markDate) {
-                    reminderTemplate(date, len)
-                }
-            })
-        }
-    }
-
-
-}
-
-// data = {unixTime: []}
-const ttt = () => {
-    const data = {}
-    globalData.forEach( g => {
-        const time = g.unixTime
-        if (data[time]) {
-            const task = data[time]
-            task.push(g.reminder)
-        } else {
-            data[time] = [g.reminder]
-        }
-    } )
-
-    return data
-}
-
-const loadReminderMark = () => {
-    const data = []
-    for (let d of globalData) {
-        const time = d.unixTime
-        if (time) {
-           const index = data.findIndex( d => d === time )
-            if (index) {
-                data.push(time)
-            }
-        }
-    }
-    return data
-}
-
-const dateByHighlight = () => {
-    const {year, month, date} = dateUpdate()
-    const unixTime = new Date(year, month, date).getTime()
-    let data = []
-    for (let d of globalData) {
-        if ( d.unixTime === unixTime && d.reminder ) {
-            let t = d.reminder
-            data.push(t)
-        }
-    }
-    return data
-
-    /*
-    for (let d of database) {
-        if ( d.unixTime === unixTime && d.task ) {
-                let t = d.task
-                if (t['_deleted'] === false) {
-                    data.push(d)
-            }
-        }
-    }
-    */
-}
-
-const loadReminder = () => {
-    const {todoContainer} = todoElementContainer()
-    todoContainer.innerHTML=''
-
-    const data = dateByHighlight()
-    // console.log('data', data)
-    if (data) {
-        data.forEach( d => {
-            insertTodoTemplate(d)
-        })
-    }
-
-}
-
-
-
-const retrieveFile = () => {
-    const file = localStorage.calendar
-    let data = []
-    if (file !== undefined) {
-        data = JSON.parse(file)
-    }
-    return data
-}
-
-const saveData = (data) => {
-    const {year, month, date} = dateUpdate()
-    const unixTime = new Date(year, month, date).getTime()
-
-    const d =  {
-        unixTime: unixTime,
-        'reminder': {
-                    'task': data.task ,
-                    'done': data.done,
-                },
-    }
-
-    globalData.push(d)
-    // console.log('globalData',globalData)
-
-    localStorage.calendar = JSON.stringify(globalData)
-}
-
 const todoEvent = () => {
     addContainerToggle()
-    todoContentAdd()
-    todoContentEdit()
+    todoAdd()
+    todoUpdate()
 }
 
 
-// globalData = [
-//     {
-//         unixTime: 1520582400000,
-//         reminder: {
-//             'task': 'ojbk',
-//             done: true,
-//         }
-//     },
-//     {
-//         unixTime: 1520668800000,
-//         reminder: {
-//             'task': 'ojsssbk',
-//             done: true,
-//         }
-//     },
-//     {
-//         unixTime:1521788400000,
-//         reminder: {
-//             'task': 'testtest',
-//             done: true,
-//         }
-//     },
-//
-// ]
-
-const __main = () => {
-    globalData = retrieveFile()
-    init()
-    highlightEvent()
-    todoEvent()
-
-    // calendarEvents()
-
-}
-
-__main()
